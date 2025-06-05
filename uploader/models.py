c021af378ffa6e939036c4aef6232f402baf92a6
Ravi kant <kantravi2025m@gmail.com>
2025-06-05 23:49:00 +0530

    removed unwanted files

--- uploader/models.py
+++ uploader/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+import os
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
+
+
 class Upload(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='uploads/')
@@ -29,3 +35,31 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username} on {self.upload.id}: {self.text[:20]}"
+    
+
+
+
+
+
+# Delete image file from disk when Upload is deleted
+@receiver(post_delete, sender=Upload)
+def delete_upload_image_file(sender, instance, **kwargs):
+    if instance.image and os.path.isfile(instance.image.path):
+        os.remove(instance.image.path)
+
+# Delete old image file when it's replaced during edit
+@receiver(pre_save, sender=Upload)
+def delete_old_image_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return  # New object, nothing to delete
+
+    try:
+        old_image = Upload.objects.get(pk=instance.pk).image
+    except Upload.DoesNotExist:
+        return
+
+    new_image = instance.image
+    if old_image and old_image != new_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
+
